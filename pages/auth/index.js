6b1bb191031d6c7add57f63f428dcc11b4fdfe59
QdabuliuQ <2714499297@qq.com>
2020-12-02 09:11:22 +0800

    个人页面布局

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -11,6 +11,11 @@ Page({
 
   // 获取用户信息
   getUserInfo(e){
+    let userInfo = e.detail.userInfo  // 获取用户信息
+    wx.setStorageSync('userinfo', userInfo)  // 保存到缓存
+    wx.navigateBack({  // 返回上一个页面
+      delta: 1,
+    })
     // // 获取用户参数
     // const {encryptedData, rawData, iv, signature} = e.detail
     // // 微信登录授权
@@ -34,8 +39,8 @@ Page({
     //     })
     //   }
     // })
-    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo"
-    wx.setStorageSync('token', token)  // 保存到缓存
+    // const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo"
+    // wx.setStorageSync('token', token)  // 保存到缓存
   },
   /**
    * 生命周期函数--监听页面加载
--- pages/user/index.js
+++ pages/user/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    isUserDetail: false
   },
 
   /**
@@ -16,51 +17,25 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    const userInfo = wx.getStorageSync('userinfo')||null  // 获取缓存中的用户数据
+    if (userInfo == null) {
+      this.setData({
+        isUserDetail: false
+      })
+    } else {
+      this.setData({
+        userInfo,
+        isUserDetail: true
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toLogin(){
+    wx.navigateTo({
+      url: '/pages/auth/index',
+    })
   }
 })
